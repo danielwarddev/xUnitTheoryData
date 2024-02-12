7df58390a5d2c3708be7a3d862ece8eed220a64a
Daniel Ward <dward256@hotmail.com>
2024-02-12 16:21:53 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# xUnitTheoryData
+
+This is the example project for the following post: [Create clean test cases in xUnit with TheoryData](https://daninacan.com/create-clean-test-cases-in-xunit-with-theorydata). Please feel free to leave any questions and comments on that post.
---
+++ xUnitTheoryData.UnitTests/FileLineParserTests.cs
@@ -0,0 +1,58 @@
+using FluentAssertions;
+
+namespace xUnitTheoryData.UnitTests;
+
+public class FileLineParserTests
+{
+    private readonly FileLineParser _parser = new();
+    
+    public static TheoryData<string, LineData> HappyTestCases = new()
+    {
+        {
+            "[Very Cool Book] Bob, Daniel Ward, Julia Childs; 2003; en, es, fn; Drama/Fiction/History",
+            new LineData
+            (
+                "Very Cool Book",
+                new string[] { "Bob", "Daniel Ward", "Julia Childs" },
+                2003,
+                new string[] { "en", "es", "fn" },
+                new string[] { "Drama", "Fiction", "History" }
+            )
+        },
+        {
+            "[      Yet Another Book     ] Person, George Last-Name, King Charles Jr.; 100; ; ",
+            new LineData
+            (
+                "Yet Another Book",
+                new string[] { "Person", "George Last-Name", "King Charles Jr." },
+                100,
+                new string[] { string.Empty },
+                new string[] { string.Empty }
+            )
+        }
+    };
+
+    public static TheoryData<string> SadTestCases = new()
+    {
+        "[Book name without a closing bracket Bob, Daniel Ward, Julia Childs; 2003; en, es, fn; Drama/Fiction/History",
+        "[Line with ] too many; semicolons; Bob, Daniel Ward, Julia Childs; 2003; en, es, fn; Drama/Fiction/History",
+        "Line missing data",
+        ""
+    };
+
+    [Theory]
+    [MemberData(nameof(HappyTestCases))]
+    public void Line_Is_Parsed_When_Formatted_Correctly(string line, LineData expectedLineData)
+    {
+        var actualLineData = _parser.ParseLine(line);
+        actualLineData.Should().BeEquivalentTo(expectedLineData);
+    }
+
+    [Theory]
+    [MemberData(nameof(SadTestCases))]
+    public void Throws_Exception_When_Line_Is_Malformatted(string line)
+    {
+        var parseLine = () => _parser.ParseLine(line);
+        parseLine.Should().Throw<Exception>();
+    }
+}
